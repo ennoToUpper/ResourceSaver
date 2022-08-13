fb44e532a2f296116c1a3a1e93e33aee3898ef86
Andrew73174 <andrew-chamberlain@gmx.net>
2022-08-13 19:46:24 +0200

    Fixed solution and added output of settings file path

--- ResourceSaver/Settings.cs
+++ ResourceSaver/Settings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using minecraftstarter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,24 +17,27 @@ namespace ResourceSaver
 
         internal static void LoadSettings()
         {
-            using (var stream = new StreamReader("settings.txt"))
-            {
-                while (!stream.EndOfStream)
-                {
-                    var lineSplit = stream.ReadLine()?.Split('>');
+            using var stream = new StreamReader("settings.txt");
 
-                    if (lineSplit?.Length > 1)
+            string path = ((FileStream)stream.BaseStream)?.Name ?? "File not found";
+
+            WriteLog.WriteLogMessage(path);
+
+            while (!stream.EndOfStream)
+            {
+                var lineSplit = stream.ReadLine()?.Split('>');
+
+                if (lineSplit?.Length > 1)
+                {
+                    string clean = lineSplit[1].Replace(" ", "");
+                    switch (lineSplit[0])
                     {
-                        string clean = lineSplit[1].Replace(" ", "");
-                        switch (lineSplit[0])
-                        {
-                            case "Port": port = int.Parse(clean); break;
-                            case "Server RAM": arguments = clean; break;
-                            case "Jar Name": jarname = clean; break;
-                            case "Server Directory": directory = clean; break;
-                            case " HTTP Keyword": keyword = clean; break;
-                            default: break;
-                        }
+                        case "Port": port = int.Parse(clean); break;
+                        case "Server RAM": arguments = clean; break;
+                        case "Jar Name": jarname = clean; break;
+                        case "Server Directory": directory = clean; break;
+                        case " HTTP Keyword": keyword = clean; break;
+                        default: break;
                     }
                 }
             }
